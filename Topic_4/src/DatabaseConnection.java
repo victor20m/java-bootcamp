b975bad3c3db1c87767d66be55b7ceb62ca65660
Victor <victormunareto@gmail.com>
2018-10-09 19:56:11 -0300

    added topic4

--- Topic_4/src/DatabaseConnection.java
+++ Topic_4/src/DatabaseConnection.java
@@ -1,25 +1,27 @@
 import java.sql.*;
+
 public class DatabaseConnection {
-	public static void main (String args[]) {
-		
+	public static void main(String args[]) {
+
 		Connection dtbCon = null;
-		
+
 		try {
-			dtbCon = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/world?autoReconnect=true&useSSL=false","java","123");
+			dtbCon = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/world?autoReconnect=true&useSSL=false",
+					"java", "123");
 			Statement stat = dtbCon.createStatement();
-			ResultSet myRes = stat.executeQuery("SELECT CONCAT(t.last_name_teacher, \", \", t.first_name_teacher) as Teacher,\r\n" + 
-					"  CONCAT(c.Schedule_time, \" \", c.course_name) as Schedule\r\n" + 
-					"  FROM Course as c, teacher as t\r\n" + 
-					"  WHERE t.idTeachers = 3\r\n" + 
-					"  GROUP BY teacher;");
-			
-			while(myRes.next()) {
-				
+			ResultSet myRes = stat
+					.executeQuery("SELECT CONCAT(t.last_name_teacher, \", \", t.first_name_teacher) as Teacher,\r\n"
+							+ "CONCAT(c.Schedule_time, \" \", c.course_name) as Schedule\r\n"
+							+ "FROM Course as c, teacher as t\r\n" + "WHERE t.idTeachers = 3\r\n"
+							+ "GROUP BY teacher\r\n" + "ORDER BY c.Schedule_time ASC;");
+
+			while (myRes.next()) {
+
 				System.out.print(myRes.getString("Teacher"));
-				System.out.println(" "+myRes.getString("Schedule"));
+				System.out.println(" " + myRes.getString("Schedule"));
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			System.out.println(e);
 			e.printStackTrace();
 		}
